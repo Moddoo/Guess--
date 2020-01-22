52192e49f6ff76d1acebbb8544a7f17ab57ddcac
Moddoo <zoddoo@yahoo.com>
2020-01-22 17:15:11 -0600

    50%

---
+++ assets/main.js
@@ -0,0 +1,63 @@
+let trials = document.getElementById("trials");
+let inputs = document.getElementById("inputs");
+let one    = document.getElementById("p1");
+let hint   = document.getElementById("hint");
+let next   = document.getElementById("next");
+let put    = document.getElementById("put");
+
+let app = {
+    
+    words:    [
+        {
+            w: "APPLE"
+        },
+        {
+            w: "BEACH"  
+        },
+        {
+            w: "WATER"  
+        },
+        {
+            w: "WOMAN"
+        },
+        {
+            w: "CLOUD"  
+        },
+        {
+            w: "EARTH"  
+        }
+    ],
+    
+    index:    0,
+    
+    trial:    10,
+    
+    load:     function() {
+        if(app.words[app.index].w.indexOf(put.value.trim().toUpperCase()) !== -1 && put.value.trim()) {
+            y = one.value.split(" ");
+            z = app.words[app.index].w.split("");
+            for(i=0; i<z.length; i++){
+                if(z[i] == put.value.trim().toUpperCase()) {
+                    y[i] = z[i];
+                    console.log(z);
+                }
+                
+            }
+            
+            x = y.join(" ");
+            one.value = x;
+            console.log(y);
+            console.log(x);
+        } else {
+            app.trial--;
+            trials.textContent = "Trials: " + app.trial;
+        }
+        
+        put.value = "";
+        
+    } 
+    
+}
+put.addEventListener("input", app.load);
+trials.textContent = "Trials: " + app.trial;
+// app.load();
